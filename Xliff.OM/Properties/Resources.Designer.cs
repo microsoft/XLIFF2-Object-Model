13275d030fe37abf4b4975b0a99e05e8f94e8494
simonech <simone@piyosailing.com>
2019-05-13 22:07:33 +0200

    Converted library to NetStandard 2.0

--- Xliff.OM/Properties/Resources.Designer.cs
+++ Xliff.OM/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Localization.Xliff.OM.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Localization.Xliff.OM.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Xliff.OM.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
